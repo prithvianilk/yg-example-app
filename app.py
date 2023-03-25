727d9b473a5856eac16c86ac04674d5010a53b6e
prithvianilk <thepirateking3d2y@gmail.com>
2023-03-25 18:43:12 +0530

    Init commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+dishes = []
+
+
+@app.route('/dish', methods=["GET", "POST"])
+def get():
+    if request.method == "GET":
+        return jsonify(dishes)
+    elif request.method == "POST":
+        dishes.append(request.get_json())
+        return "OK", 200
+
+
+if __name__ == '__main__':
+    app.run()
